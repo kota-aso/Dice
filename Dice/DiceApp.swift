5d0df65b5c0905776e62a1cf3c65cb9f3544f554
kota-aso <kotaaso1212@gmail.com>
2024-03-19 20:34:22 +0900

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Kota Aso on 2024/03/19.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
