91ad0318773ce50e88ae8f527b91fa638a5b25b1
kota-aso <kotaaso1212@gmail.com>
2024-03-19 22:00:52 +0900

    ボタンを押したらサイコロの目が変わる

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -8,18 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var randomNumber = 1
     var body: some View {
         VStack {
-            Image(systemName: "die.face.1")
+            Spacer()
+            Image(systemName: "die.face.\(randomNumber)")
                 .resizable()
                 .scaledToFit()
                 .frame(width: UIScreen.main.bounds.width/2)
                 .padding()
+            Spacer()
             Button(action: {
                 print("ボタンが押されたよ")
+                randomNumber = Int.random(in: 1...6)
             }){
               Text("サイコロを振る")
+                    .padding()
+                    .background(.orange)
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
             }
+            Spacer()
         }
     }
 }
