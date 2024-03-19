1da4ce676e3d26b4cb6c98fb70ff1fd1bce0b9ee
kota-aso <kotaaso1212@gmail.com>
2024-03-19 21:37:53 +0900

    firstcommit

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(systemName: "die.face.1")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width/2)
+                .padding()
+            Button(action: {
+                print("ボタンが押されたよ")
+            }){
+              Text("サイコロを振る")
+            }
         }
-        .padding()
     }
 }
 
